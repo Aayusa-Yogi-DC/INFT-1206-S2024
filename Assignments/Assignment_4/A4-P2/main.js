cfb80924393526df3973ee40103872adc43e2961
aayusa <aayusayogi60@gmail.com>
2024-08-09 00:10:34 -0400

    A4-P2 (3rd commit)
    Added loops and included a handler that will lighten/darken the button.

--- Assignments/Assignment_4/A4-P2/main.js
+++ Assignments/Assignment_4/A4-P2/main.js
@@ -9,10 +9,26 @@ const overlay = document.querySelector('.overlay');
 /* Declaring the alternative text for each image file */
 
 /* Looping through images */
+for (let i = 0; i < imageFilenames.length; i++) {
+    const newImage = document.createElement('img');
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+    newImage.setAttribute('src', 'images/${imageFilenames[i]}');
+    newImage.setAttribute('alt', altTexts);
+    thumbBar.appendChild(newImage);
+
+}
+;
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', function() {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+        btn.setAttribute('class', 'light');
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)'; 
+    }else{
+        btn.setAttribute('class', 'dark');
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+});
