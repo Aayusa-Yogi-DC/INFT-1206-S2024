f6cd445a1ddc256bd59d9026741a9b87900c42de
aayusa <aayusayogi60@gmail.com>
2024-08-09 00:13:54 -0400

    A4-P2 (4th commit)
    Declaring the array of image filenames

--- Assignments/Assignment_4/A4-P2/main.js
+++ Assignments/Assignment_4/A4-P2/main.js
@@ -5,6 +5,8 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imageFilenames = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
+
 
 /* Declaring the alternative text for each image file */
 
@@ -16,8 +18,12 @@ for (let i = 0; i < imageFilenames.length; i++) {
     newImage.setAttribute('alt', altTexts);
     thumbBar.appendChild(newImage);
 
+    // Adding click event to update the displayed image when thumbnail is clicked
+  newImage.addEventListener('click', function() {
+    displayedImage.setAttribute('src', `images/${imageFilenames[i]}`);
+    displayedImage.setAttribute('alt', altTexts[i]);
+  });
 }
-;
 
 /* Wiring up the Darken/Lighten button */
 btn.addEventListener('click', function() {
